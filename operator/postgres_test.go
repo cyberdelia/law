7a25ec6dc46f77f7b4d5440d21adcc4656cbce39
Timothée Peignier <timothee.peignier@tryphon.org>
2014-09-19 15:14:31 -0700

    read database env var

--- operator/postgres_test.go
+++ operator/postgres_test.go
@@ -1,12 +1,13 @@
 package operator
 
 import (
+	"os"
 	"strings"
 	"testing"
 )
 
 func TestVersion(t *testing.T) {
-	db, err := NewDatabase("")
+	db, err := NewDatabase(os.Getenv("DATABASE_URL"))
 	if err != nil {
 		t.Fatal(err)
 	}
